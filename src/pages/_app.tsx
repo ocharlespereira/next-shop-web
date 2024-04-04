f2754408d03227127057129f4a1dba0f927cbb3e
CharlesPereira <charlesti.ptu@gmail.com>
2024-04-03 23:34:20 -0300

    feat: header apllication default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
 import { globalStyles } from "@/styles/global";
 import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  globalStyles();
+import logoImage from "@/assets/logo.svg";
+import { Container, Header } from "@/styles/pages/app";
 
-  return <Component {...pageProps} />;
+globalStyles();
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImage.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
+
+export default App;
