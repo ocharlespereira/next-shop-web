be48598feeb2ac884c66f63961d81c2cf0e7b45a
CharlesPereira <charlesti.ptu@gmail.com>
2024-04-03 23:24:27 -0300

    feat: create project next-web

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { globalStyles } from "@/styles/global";
+import { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
+
+  return <Component {...pageProps} />;
+}
---
+++ src/styles/index.ts
@@ -0,0 +1,28 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  config,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      primary: "#8257e6",
+
+      white: "#FFF",
+
+      gray900: "#121214",
+      gray800: "#202024",
+      gray300: "#c4c4cc",
+      gray100: "#e1e1e6",
+
+      green500: "#00875f",
+      green300: "#00b37e",
+    },
+  },
+});
