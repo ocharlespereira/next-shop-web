508510983b3b6adff9844741138255aefe15e13f
CharlesPereira <charlesti.ptu@gmail.com>
2024-04-09 20:45:21 -0300

    feat: navigation link with parameters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { HomeContainer, Product } from '@/styles/pages/home';
 import Image from 'next/image';
 import { stripe } from '@/lib/stripe';
 import { GetServerSideProps, GetStaticProps } from 'next';
+import Link from 'next/link';
 
 import { useKeenSlider } from 'keen-slider/react';
 
@@ -33,14 +34,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product key={product.id} className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>R$ {product.price.toFixed(2)}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>R$ {product.price.toFixed(2)}</span>
+              </footer>
+            </Product>
+          </Link>
         );
       })}
     </HomeContainer>
