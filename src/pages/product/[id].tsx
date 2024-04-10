475cb14fcf6e23a7fef52cd227e79f17de1261f5
CharlesPereira <charlesti.ptu@gmail.com>
2024-04-09 21:00:26 -0300

    feat: style product details

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,31 @@
-import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export default function Product() {
   const { query } = useRouter();
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image /> */}</ImageContainer>
+
+      <ProductDetails>
+        <h1>T-shirt</h1>
+        <span>R$ 178,90</span>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          rem eos pariatur molestiae, facere sint reiciendis odio illo libero
+          tenetur doloremque neque beatae iure quisquam nihil alias, laborum
+          harum dolore!
+        </p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
