eb71cd2c65d4e4c47873879fbef5d36db7fc4bcc
CharlesPereira <charlesti.ptu@gmail.com>
2024-04-10 23:44:35 -0300

    feat: SSG payload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ interface HomeProps {
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[];
 }
 
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -5,27 +5,63 @@ import {
 } from '@/styles/pages/product';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { stripe } from '@/lib/stripe';
+import { GetStaticProps } from 'next';
+import Stripe from 'stripe';
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
 
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
-      <ImageContainer>{/* <Image /> */}</ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>T-shirt</h1>
-        <span>R$ 178,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-          rem eos pariatur molestiae, facere sint reiciendis odio illo libero
-          tenetur doloremque neque beatae iure quisquam nihil alias, laborum
-          harum dolore!
-        </p>
+        <p>{product.description}</p>
 
         <button>Comprar Agora</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}: any) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        nme: product.name,
+        imageUrl: product.images[0],
+        price: new intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
+};
